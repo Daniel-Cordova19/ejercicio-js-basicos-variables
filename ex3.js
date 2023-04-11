499f4b8efc9d4c1e84833bfbadc76b9da7ac3022
Daniel Cordova <javierdanielcordova@gmail.com>
2023-04-11 12:53:00 +0200

    ejercicio 3

--- ex3.js
+++ ex3.js
@@ -2,15 +2,18 @@
  * Jugando con string
  */
 
-let nombre = "Mad";
+let nombre = "Mad ";
 let apellido = "Max";
+let presentacion = "Me llamo " + nombre + apellido;
 
-let presentacion = "";
+
+
 
 /**
  * Pregunta 1: ¿Qué valor tengo que asignar a 'presenetacion' para que me muestre pantalla:
  *   * "Me llamo Mad Max?"
- *
+ * 
+ *Respuesta 1: hay que agregar una operación con el signo + añadiendo la variable nombre + la variable apellido
  * 
  * 
  */
@@ -19,6 +22,11 @@ console.log("Pregunta 1:", presentacion);
 
 /**
  * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
+ * Respuesta 1:
  */
+
+nombre = "Daniel ";
+apellido = "Cordova";
+presentacion = "Me llamo " + nombre + apellido;
 
 console.log("Pregunta 2:", presentacion);
